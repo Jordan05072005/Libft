ffd35c8f357eb78a69b020933463b9799f2a15cb
Jordan <guaglio.jordan@gmail.com>
2024-10-05 15:29:09 +0200

    first commit before correction

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jguaglio <guaglio.jordan@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 13:06:22 by jguaglio          #+#    #+#             */
+/*   Updated: 2024/09/10 13:06:22 by jguaglio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*dst;
+
+	if (!((nmemb == 0) == (size == 0)))
+		return (NULL);
+	dst = malloc(nmemb * size);
+	if (!dst)
+		return (NULL);
+	ft_bzero(dst, nmemb * size);
+	return (dst);
+}
